95010508fbcca69693357faa5e588d9b2b99a28d
black <joseb.twelve@gmail.com>
2024-05-13 15:43:33 -0500

    feat: added package.json to start script

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,4 @@ source venv/bin/activate
 cd backend
 pip install -re requirements.txt
 cd ..
-export NODE_PATH=/usr/lib/node_modules
 ./start.sh
--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 ./run-fastapi.sh
-HOST=143.198.237.241 PORT=80 node ./run-astro.mjs
+
+pnpm i
+pnpm start
