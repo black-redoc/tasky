9116ebc5226662370d7cc3ce6bbdeb8b5e051c26
black <joseb.twelve@gmail.com>
2024-06-02 20:38:31 -0500

    feat: added express static public files

--- run-astro.mjs
+++ run-astro.mjs
@@ -1,10 +1,10 @@
-import express from 'express';
-import { handler as ssrHandler } from './dist/server/entry.mjs'
+import express from "express";
+import { handler as ssrHandler } from "./dist/server/entry.mjs";
 
-const app = express()
-const base = '/'
-app.use(express.static("public"));
-app.use(base, express.static('dist/client'))
+const app = express();
+const base = "/";
+app.use("public", express.static("public"));
+app.use(base, express.static("dist/client"));
 app.use(ssrHandler);
-const PORT = process.env.PORT || 3000
-app.listen(PORT)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
