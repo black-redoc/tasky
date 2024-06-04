08f570b1e9fae32c44dcf0915ec7fa3d7ee518f9
black <joseb.twelve@gmail.com>
2024-06-04 15:07:29 -0500

    feat: added auth store and auth services

---
+++ src/components/login-form.tsx
@@ -0,0 +1,133 @@
+import { useState } from "react";
+import { md5 } from "js-md5";
+import Toast from "./toast";
+import { login as loginService } from "../services/auth.service";
+import { setToastMessage } from "../store/toast.store";
+import { loginStore } from "../store/auth.store";
+
+export default () => {
+  const ERROR_COLOR = "text-red-600";
+  const HINT_COLOR = "text-cyan-500";
+  const [inputHintEmail, setInputHintEmail] = useState("");
+  const [inputHintPassword, setInputHintPassword] = useState("");
+  const [hintColor, setHintColor] = useState(ERROR_COLOR);
+  const [inputEmail, setInputEmailValue] = useState("");
+  const [inputPassword, setInputPasswordValue] = useState("");
+  const onChangeInputEmailValue = ({ target }: { target: HTMLInputElement }) =>
+    setInputEmailValue(target.value);
+  const onChangeInputPasswordValue = ({
+    target,
+  }: {
+    target: HTMLInputElement;
+  }) => setInputPasswordValue(target.value);
+  const onSubmit = async (event: React.FormEvent<HTMLElement>) => {
+    event.preventDefault();
+    if (!inputEmail) {
+      setInputHintEmail("Email cannot be empty!");
+      setHintColor(ERROR_COLOR);
+      return;
+    }
+
+    if (!inputEmail.match(/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/g)) {
+      setInputHintEmail("Please enter a valid email address!");
+      setHintColor(HINT_COLOR);
+      return;
+    }
+    setInputHintEmail("");
+    if (!inputPassword) {
+      setInputHintPassword("Password cannot be empty!");
+      setHintColor(ERROR_COLOR);
+      return;
+    }
+    setInputHintPassword("");
+    const response = await loginService({
+      email: inputEmail,
+      password: md5(inputPassword),
+    });
+    if ("error" in response) {
+      setToastMessage({ message: response.error, isError: true });
+      return;
+    }
+    loginStore({ ...response });
+    window.location.replace("/")
+  };
+  return (
+    <>
+      <Toast />
+      <form className="bg-slate-200 h-[25rem] rounded-b-lg relative w-[22rem] flex flex-col items-center">
+        <h1
+          className={`
+        bg-cyan-700
+        text-slate-100
+        font-semibold
+        py-2
+        px-5
+        rounded-t-lg w-full
+        `}
+        >
+          Login
+        </h1>
+        <section className="mt-12">
+          <p
+            className={`
+          ${hintColor} mb-1 text-wrap
+          mx-4
+          `}
+          >
+            <input
+              type="email"
+              placeholder="email"
+              autoFocus
+              defaultValue={inputEmail}
+              onChange={onChangeInputEmailValue}
+              className={`
+            w-[19rem] text-gray-600 font-normal
+            focus:ring-cyan-600 ring-inset
+            rounded-md block border-0 ring-1
+            focus:ring-2 focus:ring-inset leading-6
+            focus:border-none appearance-none
+            outline-none px-5 py-1 h-10
+            `}
+            />
+            &nbsp;{inputHintEmail}
+          </p>
+          <p
+            className={`
+          ${hintColor} mb-2 text-wrap mx-4
+          `}
+          >
+            <input
+              type="password"
+              placeholder="password"
+              autoFocus
+              defaultValue={inputPassword}
+              onChange={onChangeInputPasswordValue}
+              className={`
+            w-[19rem] text-gray-600 font-normal
+            focus:ring-cyan-600 ring-inset
+            rounded-md block border-0 ring-1
+            focus:ring-2 focus:ring-inset leading-6
+            focus:border-none appearance-none
+            outline-none px-5 py-1 h-10
+            `}
+            />
+            &nbsp;{inputHintPassword}
+          </p>
+        </section>
+        <section className="absolute bottom-5">
+          <button
+            onClick={onSubmit}
+            className={`
+          bg-cyan-700 hover:bg-sky-500 w-[19rem]
+          py-2 mx-3
+          text-white font-semibold
+          text-xl rounded-xl shadow-xl
+          `}
+          >
+            Submit
+          </button>
+        </section>
+      </form>
+    </>
+  );
+};
