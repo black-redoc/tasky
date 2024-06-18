e039e6f1bfa65d6f1cf937cd0d68301061d7bb81
black <joseb.twelve@gmail.com>
2024-06-17 23:15:44 -0500

    feat: added tests to auth service

---
+++ src/app/services/tests/auth.service.test.ts
@@ -0,0 +1,40 @@
+import { describe, expect, it, vi, beforeEach, afterEach, Mock } from "vitest";
+import { loadEnvConfig } from "@next/env";
+import { login, validateUsername } from "../auth.service";
+
+describe("auth.service", () => {
+  beforeEach(() => {
+    const projectDir = process.cwd();
+    loadEnvConfig(projectDir);
+  });
+
+  afterEach(() => {
+    // restoring date after each test run
+    vi.useRealTimers();
+  });
+
+  it("should login", async () => {
+    const expected_value = "Success";
+    global.fetch = vi.fn(() =>
+      Promise.resolve({ json: () => expected_value })
+    ) as Mock;
+    const mock = vi.fn().mockImplementation(login);
+    mock.mockResolvedValue(expected_value);
+    const res = await login({
+      email: "test@test.com",
+      password: "test",
+    });
+    expect(res).toBe(expected_value);
+  });
+
+  it("should validate username", async () => {
+    const expected_value = "Success";
+    global.fetch = vi.fn(() =>
+      Promise.resolve({ json: () => expected_value })
+    ) as Mock;
+    const mock = vi.fn().mockImplementation(validateUsername);
+    mock.mockResolvedValue(expected_value);
+    const res = await validateUsername({ username: "test" });
+    expect(res).toBe(expected_value);
+  });
+});
