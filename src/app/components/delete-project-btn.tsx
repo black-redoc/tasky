83f3d6d391aeb625840c577875696057e4a49d8c
black <joseb.twelve@gmail.com>
2024-06-24 18:39:15 -0500

    feat: added tests to projects component

--- src/app/components/delete-project-btn.tsx
+++ src/app/components/delete-project-btn.tsx
@@ -56,6 +56,7 @@ export default function DeleteProjectBtn({
         <></>
       )}
       <svg
+        role="button"
         onClick={deleteProject}
         xmlns="http://www.w3.org/2000/svg"
         width="24"
