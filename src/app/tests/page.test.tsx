02a980bebdba314cafca5fc353a73a49975c861d
black <joseb.twelve@gmail.com>
2024-07-24 18:08:40 -0500

    fix: fixed tests

--- src/app/tests/page.test.tsx
+++ src/app/tests/page.test.tsx
@@ -1,10 +1,13 @@
 import { render } from "@testing-library/react";
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi, Mock } from "vitest";
 import Index from "../page";
 import { StateProvider } from "../providers/state_provider";
 
 describe("Index page", () => {
   it("renders", () => {
+    global.fetch = vi.fn(() =>
+      Promise.resolve({ json: () => "success", status: 200 })
+    ) as Mock;
     render(
       <StateProvider>
         <Index />
